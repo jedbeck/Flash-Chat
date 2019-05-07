55c97228bc1a706c77d0ad24f781f93dfbd14681
Jason Becker <jbecker@newsbank.com>
2019-05-07 13:37:19 -0400

    Commit from finished Flash Chat app with some additions of making the keyboard send button work and scrolling to the newest message

---
+++ Flash Chat/ChatViewController.swift
@@ -0,0 +1,263 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Angela Yu on 29/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+import ChameleonFramework
+
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    // Declare instance variables here
+    var messageArray : [Message] = [Message]()
+    
+    
+    // We've pre-linked the IBOutlets
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var messageTextfield: UITextField!
+    @IBOutlet var messageTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO: Set yourself as the delegate and datasource here:
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        
+        
+        
+        //TODO: Set yourself as the delegate of the text field here:
+        messageTextfield.delegate = self
+        
+        
+        //TODO: Set the tapGesture here:
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
+
+        //TODO: Register your MessageCell.xib file here:
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        configureTableView()
+        retrieveMessages()
+        
+        messageTableView.separatorStyle = .none
+        
+        
+    }
+
+    ///////////////////////////////////////////
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    
+    //TODO: Declare cellForRowAtIndexPath here:
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
+        cell.senderUsername.textColor = UIColor.flatWhite()
+        
+        if cell.senderUsername.text == Auth.auth().currentUser?.email as String! {
+            // Mesasages we send
+            cell.avatarImageView.backgroundColor = UIColor.flatMint()
+            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
+        }
+        else {
+            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon()
+            cell.messageBackground.backgroundColor = UIColor.flatGray()
+        }
+        
+        
+        return cell
+    }
+    
+    private func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        let lastRowIndex = messageTableView.numberOfRows(inSection: 0)
+        if indexPath.row == lastRowIndex - 1 {
+            messageTableView.scrollToBottom(animated: true)
+        }
+    }
+    
+    
+    //TODO: Declare numberOfRowsInSection here:
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return messageArray.count
+        
+    }
+    
+    
+    //TODO: Declare tableViewTapped here:
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
+    
+    
+    //TODO: Declare configureTableView here:
+    func configureTableView() {
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
+    
+    
+    ///////////////////////////////////////////
+    
+    //MARK:- TextField Delegate Methods
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 358
+            self.view.layoutIfNeeded()
+            self.messageTableView.scrollToBottom()
+        }
+    }
+    
+    
+
+    
+    //TODO: Declare textFieldDidBeginEditing here:
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+            self.messageTableView.scrollToBottom()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.messageTextfield {
+            textField.resignFirstResponder()
+            
+            sendPressed(self)
+            
+            return false
+            
+        }
+        print("There")
+        return true
+    }
+    
+    
+    
+    //TODO: Declare textFieldDidEndEditing here:
+    
+
+    
+    ///////////////////////////////////////////
+    
+    
+    //MARK: - Send & Recieve from Firebase
+    
+    
+    
+    
+    
+    @IBAction func sendPressed(_ sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
+        messageTextfield.endEditing(true)
+        //TODO: Send the message to Firebase and save it in our database
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextfield.text!]
+        
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, reference) in
+            
+            if error != nil {
+                print(error!)
+            }
+            else {
+                print("Message saved successfully!")
+                
+                SVProgressHUD.dismiss()
+                
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextfield.text = ""
+                self.messageTableView.scrollToBottom()
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    //TODO: Create the retrieveMessages method here:
+    func retrieveMessages() {
+        
+        let messageDB = Database.database().reference().child("Messages")
+        
+        messageDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String,String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            
+            self.configureTableView()
+            self.messageTableView.reloadData()
+            
+            self.messageTableView.scrollToBottom()
+        }
+        
+    }
+    
+
+    
+    
+    
+    @IBAction func logOutPressed(_ sender: AnyObject) {
+        
+        //TODO: Log out the user and send them back to WelcomeViewController
+        do {
+            try Auth.auth().signOut()
+            
+            navigationController?.popToRootViewController(animated: true)
+        }
+        catch {
+            print("Error, There was a problem signing out")
+        }
+        
+        
+    }
+    
+
+
+}
+
+extension UITableView {
+    func scrollToBottom(animated: Bool = true) {
+        let sections = self.numberOfSections
+        let rows = self.numberOfRows(inSection: sections - 1)
+        if (rows > 0){
+            self.scrollToRow(at: NSIndexPath(row: rows - 1, section: sections - 1) as IndexPath, at: .bottom, animated: true)
+        }
+    }
+}
